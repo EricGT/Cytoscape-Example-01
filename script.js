009c13f0d5c788b02019f06a8c59160abaed26ce
Eric Taucher <Researcher0x00@gmail.com>
2020-08-23 17:51:05 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+// Code goes here
+
+var CanvasApp = function () {
+            var cy;
+            var playground;
+
+            return {
+                init: function () {
+                    var elem = document.createElement("div");
+                    elem.id = 'cy';
+
+                    playground = document.getElementById('playground');
+                    playground.appendChild(elem);
+
+                    cy = $(elem).cytoscape({
+                        style: [
+                            {
+                                selector: 'node',
+                                css: {
+                                    // content is needed  to display name for node
+                                    'content': 'data(name)',
+
+                                    // background-image is needed to display svg image for node
+                                    // 'background-image': 'file.svg',
+                                    'background-image': 'glucose.svg',
+
+                                    // 'background-image': 'http://png-4.findicons.com/files/icons/2015/24x24_free_application/24/text.png',
+
+
+                                    // shape is needed to give svg image for node a background area that will clip the image if the default of a circle is used.
+                                    'shape': 'rectangle',
+
+                                    // height and width are not needed to display node image
+                                    // Without height and width, size of image is incorrect.
+                                    'height': '51px',
+                                    'width': '41px',
+
+
+                                    // background-opacity is needed to hide the background shape the svg image is layered upon
+                                    'background-opacity' : '0'
+                                }
+                            }
+                        ],
+
+                        elements: {
+                            nodes: [
+                                {data: {id: 'g', name: 'Glucose'}}
+                            ],
+                            edges: []
+                        }
+                    }).cytoscape('get');
+
+                },
+
+                destroy: function () {
+                    cy.destroy();
+                    cy = null;
+                }
+            }
+        };
+
+        var app = new CanvasApp();
